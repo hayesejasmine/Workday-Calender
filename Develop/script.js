20297e89e374ee4c28c2b5748484d5aa6fb8dfda
hayesejasmine <hayesejasmine@gmail.com>
2022-11-14 23:18:50 -0500

    minor changes to html

--- Develop/script.js
+++ Develop/script.js
@@ -29,21 +29,21 @@ $(".hourly-slot").each(function() {
 // a listener for click events on the save button.
 $(".saveBtn").click(function (event) {
   event.preventDefault();
-  var value = $(this).siblings(".time-block").val();
+  var value = $(this).siblings(".time-line").val();
   var time = $(this).parent().attr("id").split("-")[1];
   localStorage.setItem(time,value);
 });
 
 
-$("#hour-09 .time-block").val(localStorage.getItem("09"));
-$("#hour-10 .time-block").val(localStorage.getItem("10"));
-$("#hour-11 .time-block").val(localStorage.getItem("11"));
-$("#hour-12 .time-block").val(localStorage.getItem("12"));
-$("#hour-13 .time-block").val(localStorage.getItem("13"));
-$("#hour-14 .time-block").val(localStorage.getItem("14"));
-$("#hour-15 .time-block").val(localStorage.getItem("15"));
-$("#hour-16 .time-block").val(localStorage.getItem("16"));
-$("#hour-22 .time-block").val(localStorage.getItem("22"));
+$("#hour-09 .time-line").val(localStorage.getItem("09"));
+$("#hour-10 .time-line").val(localStorage.getItem("10"));
+$("#hour-11 .time-line").val(localStorage.getItem("11"));
+$("#hour-12 .time-line").val(localStorage.getItem("12"));
+$("#hour-13 .time-line").val(localStorage.getItem("13"));
+$("#hour-14 .time-line").val(localStorage.getItem("14"));
+$("#hour-15 .time-line").val(localStorage.getItem("15"));
+$("#hour-16 .time-line").val(localStorage.getItem("16"));
+$("#hour-17 .time-line").val(localStorage.getItem("17"));
 
 
 $("#clear-button").click(function(event){
